5ff2fcafe4feb9f56b17849f1bf94cadc7de1413
Jeremy D. Frens <jdfrens@gmail.com>
2018-04-14 16:51:08 -0500

    Initial Elixir project

---
+++ lib/calctorio.ex
@@ -0,0 +1,18 @@
+defmodule Calctorio do
+  @moduledoc """
+  Documentation for Calctorio.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Calctorio.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/calctorio_test.exs
@@ -0,0 +1,8 @@
+defmodule CalctorioTest do
+  use ExUnit.Case
+  doctest Calctorio
+
+  test "greets the world" do
+    assert Calctorio.hello() == :world
+  end
+end
